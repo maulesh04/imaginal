2492ca000c764b2436f5aea973f227809ae9121e
maulesh04 <94596588+maulesh04@users.noreply.github.com>
2023-03-15 12:36:04 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import highLogo from './highLogo.png';
+
+export {
+  download,
+  logo,
+  preview,
+  highLogo,
+};
